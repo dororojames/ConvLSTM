004bb63edd061de26eaf26c24441fb94bdb96e2d
dororojames <30339599+dororojames@users.noreply.github.com>
2019-11-13 02:36:07 +0800

    Update model.py
    clean up code

--- model.py
+++ model.py
@@ -1,154 +1,112 @@
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
-
-sigmoid = nn.Sigmoid()
-tanh = nn.Tanh()
 
 
 class ConvLSTMCell(nn.Module):
+    """ConvLSTM cell  
+    Args:
+        in_dim (int): Number of dims of in tensor.  
+        out_shape (int, int, int): Channel height and width of in tensor as (channel, height, width).  
+        kernel_size (int, int): Size of the convolutional kernel.  
+        scale (nn.Module): Layer for scaling before Convolution
+    """
 
-    def __init__(self, shape, in_dim, hidden_dim, kernel_size, **kwargs):
-        """
-        Initialize ConvLSTM cell.
-
-        Parameters
-        ----------
-        shape: (int, int, int)
-            Channel height and width of in tensor as (channel, height, width).
-        in_dim: int
-            Number of dims of in tensor.
-        hidden_dim: int
-            Number of channels of hidden state.
-        kernel_size: (int, int)
-            Size of the convolutional kernel.
-        bias: bool
-            Whether or not to add the bias.
-        """
+    def __init__(self, in_dim, out_shape, kernel_size, scale=None, **kwargs):
         super(ConvLSTMCell, self).__init__()
+        hidden_dim, h, w = out_shape
 
-        self.channel, self.height, self.width = shape
-        self.in_dim = in_dim
-        self.hidden_dim = hidden_dim
-        self.kernel_size = kernel_size
+        self.scale = scale
+        self.conv = nn.Conv2d(in_channels=(in_dim + hidden_dim), out_channels=hidden_dim * 4,
+                              kernel_size=kernel_size, padding=kernel_size//2, **kwargs)
+        self.norm = nn.InstanceNorm2d(hidden_dim)
+        self.activation = nn.ReLU(inplace=True)
 
-        self.conv = nn.Conv2d(in_channels=(self.in_dim + self.hidden_dim) * self.channel,
-                              out_channels=self.hidden_dim * self.channel * 4,
-                              kernel_size=self.kernel_size,
-                              padding=(kernel_size[0]//2, kernel_size[1]//2),
-                              **kwargs)
+        self.h_init = torch.zeros(1, hidden_dim, h, w)
+        self.c_init = torch.zeros(1, hidden_dim, h, w)
 
-    def forward(self, x, cur_state):
+    def forward(self, X, cur_state):
+        if self.scale is not None:
+            X = self.scale(X)
         h_cur, c_cur = cur_state
 
-        # concatenate along channel axis
-        combined = torch.cat([x, h_cur], dim=1)
-        combined_conv = self.conv(combined)
+        combined_conv = self.conv(torch.cat([X, h_cur], dim=1))
+        It, Ft, Ot, Gt = combined_conv.chunk(4, dim=1)
 
-        cc_i, cc_f, cc_o, cc_g = torch.split(
-            combined_conv, self.hidden_dim * self.channel, dim=1)
-
-        i = sigmoid(cc_i)
-        f = sigmoid(cc_f)
-        o = sigmoid(cc_o)
-        g = tanh(cc_g)
-
-        c_next = f * c_cur + i * g
-        h_next = o * tanh(c_next)
+        c_next = Ft.sigmoid() * c_cur + It.sigmoid() * Gt.tanh()
+        h_next = Ot.sigmoid() * c_next.tanh()
+        h_next = self.activation(self.norm(h_next))
 
         return h_next, c_next
 
     def init_hidden(self, batch_size):
-        return (Variable(torch.zeros(batch_size, self.hidden_dim * self.channel, self.height, self.width)),
-                Variable(torch.zeros(batch_size, self.hidden_dim * self.channel, self.height, self.width)))
+        device = next(self.parameters()).device
+        return (self.h_init.repeat(batch_size, 1, 1, 1).to(device),
+                self.c_init.repeat(batch_size, 1, 1, 1).to(device))
 
 
 class ConvLSTM(nn.Module):
 
-    def __init__(self, shape, in_dim, hidden_dim, out_dim, kernel_size, batch_first=True, **kwargs):
+    def __init__(self, size, in_dim, hidden_dim, out_dim, kernel_size=3, **kwargs):
         super(ConvLSTM, self).__init__()
-
-        self._check_kernel_size_consistency(kernel_size)
-        self.shape = shape
         self.num_layers = len(hidden_dim)+1
 
-        # Make sure that both `kernel_size` and `hidden_dim` are lists having len == num_layers
+        # Make sure that `kernel_size` is lists having len == num_layers
         kernel_size = self._extend_for_multilayer(kernel_size, self.num_layers)
-        hidden_dim = self._extend_for_multilayer(hidden_dim, self.num_layers-1)
-        if not len(kernel_size) == len(hidden_dim)+1 == self.num_layers:
-            raise ValueError('Inconsistent list length.')
 
-        self.in_dim = in_dim
-        self.hidden_dim = hidden_dim
-        self.out_dim = out_dim
-        self.layer_dim = [in_dim] + hidden_dim + [out_dim]
+        dims = [in_dim] + hidden_dim + [out_dim]
 
-        self.kernel_size = kernel_size
-        self.batch_first = batch_first
-
-        self.cell_list = nn.ModuleList()
+        self.cells = nn.ModuleList()
         for i in range(self.num_layers):
-            lstmcell = ConvLSTMCell(shape=self.shape, in_dim=self.layer_dim[i], hidden_dim=self.layer_dim[i+1],
-                                    kernel_size=self.kernel_size[i], **kwargs)
-            self.cell_list.append(lstmcell)
+            lstmcell = ConvLSTMCell(in_dim=dims[i], out_shape=(dims[i+1], size[0], size[1]),
+                                    kernel_size=kernel_size[i], **kwargs)
+            self.cells.append(lstmcell)
 
-    def forward(self, x, hidden_state=None):
+    def forward(self, X, hidden=None):
         """
         Parameters
         ----------
-        x: todo 
-            5-D Tensor either of shape (t, b, c, h, w) or (b, t, c, h, w)
-        hidden_state: todo
+        X:
+            5-D Tensor (b, t, c, h, w)
+        hidden: todo
             None. todo implement stateful
 
         Returns
         -------
-        last_state_list, layer_out
+        layer_out
+
         """
-        if not self.batch_first:
-            # (t, b, c, h, w) -> (b, t, c, h, w)
-            x = x.permute(1, 0, 2, 3, 4)
 
         # Implement stateful ConvLSTM
-        if hidden_state is not None:
+        if hidden is not None:
             raise NotImplementedError()
         else:
-            hidden_state = self._init_hidden(batch_size=x.size(0))
+            hidden = self._init_hidden(batch_size=X.size(0))
 
-        layer_out_list = []
+        seq_len = X.size(1)
+        cur_layer_in = X.transpose(0, 1)
 
-        seq_len = x.size(1)
-        cur_layer_in = x
-
-        for layer_idx in range(self.num_layers):
-            h, c = hidden_state[layer_idx]
+        for l in range(self.num_layers):
+            h, c = hidden[l]
 
             out_inner = []
             for t in range(seq_len):
-                h, c = self.cell_list[layer_idx](x=cur_layer_in[:, t, :, :, :], cur_state=[h, c])
+                h, c = self.cells[l](X=cur_layer_in[t], cur_state=[h, c])
                 out_inner.append(h)
 
-            layer_out = torch.stack(out_inner, dim=1)
-            cur_layer_in = layer_out
+            cur_layer_in = out_inner
 
-            layer_out_list.append(layer_out)
-
-        return torch.split(layer_out_list[-1], split_size_or_sections=1, dim=1)[0]
+        output = h
+        return output
 
     def _init_hidden(self, batch_size):
-        init_states = []
-        for i in range(self.num_layers):
-            init_states.append(self.cell_list[i].init_hidden(batch_size))
+        init_states = [cell.init_hidden(batch_size) for cell in self.cells]
         return init_states
 
     @staticmethod
-    def _check_kernel_size_consistency(kernel_size):
-        if not (isinstance(kernel_size, tuple) or
-                (isinstance(kernel_size, list) and all([isinstance(elem, tuple) for elem in kernel_size]))):
-            raise ValueError('`kernel_size` must be tuple or list of tuples')
-
-    @staticmethod
     def _extend_for_multilayer(param, num_layers):
-        if not isinstance(param, list):
+        if isinstance(param, int):
             param = [param] * num_layers
+        assert isinstance(param, list)
+        while len(param) < num_layers:
+            param.append(param[-1])
         return param
